27bff28c3c37835cfdca181696fa0d6355ebb6b6
Unknown <mckaymanning@gmail.com>
2016-09-16 23:16:23 -0600

    VarAndFunction Assignment
    Added a new script into the Review Assignment Project.
    The script is small and contains a variable and a small function.

---
+++ Scripting Review Assignments/Assets/functionReview.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class functionReview : MonoBehaviour {
+
+	public int curHealth = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+		PowerUp ();
+	
+	}
+
+	void PowerUp()
+	{
+		curHealth++;
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		print (curHealth);
+	}
+}
