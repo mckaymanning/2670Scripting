a9f1d31085d80a142fb3932c4903b2cd721bb1e5
Unknown <mckaymanning@gmail.com>
2016-09-21 13:59:01 -0600

    Level Recycling inClass
    Changed several scripts in class with Anthony, overhauled the recycling process so the modules pull from a separate area.

--- Star Catcher/Assets/Scripts/LevelScripts/RecycleLevelModule.cs
+++ Star Catcher/Assets/Scripts/LevelScripts/RecycleLevelModule.cs
@@ -1,24 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class RecycleLevelModule : MonoBehaviour
 {
     private Vector3 newLocation;
+
+    public List<Recycler> RecyclableList;
+    private int i = 0;
+
+
     void Start()
     {
-        staticVars.nextSectionPosition = 28;
+        RecyclableList = new List<Recycler>();
+        Recycler.RecycleAction += RecycleActionHandler;
+    }
+
+    private void RecycleActionHandler(Recycler _r)
+    {
+        RecyclableList.Add(_r);
     }
 
 
-	void OnTriggerEnter()
+    // void Start()
+    //{
+    //     staticVars.nextSectionPosition = 32;
+    // }
+
+
+    void OnTriggerEnter()
     {
 			newLocation.x = staticVars.nextSectionPosition;
-			transform.position = newLocation;
+			RecyclableList[i].cube.position = newLocation;
 			staticVars.nextSectionPosition += staticVars.distance;
-		 
-
-
-		
-		}
+            if (i < RecyclableList.Count-1)
+                i++;
+    }
 
 }
