5726944c65269cf1600b6f989588bd0502199771
Unknown <mckaymanning@gmail.com>
2016-10-05 14:05:01 -0600

    Inclass Character Art Flip, Endgame
    worked on character art script, I still need to fix mine.

--- Star Catcher/Assets/Scripts/Character/CharacterFlipArt.cs
+++ Star Catcher/Assets/Scripts/Character/CharacterFlipArt.cs
@@ -8,21 +8,41 @@ public class CharacterFlipArt : MonoBehaviour
     public Transform characterArt;
     public bool forward = true;
 
-    void FlipCharacter(bool _b)
+    void FlipCharacter(KeyCode _keyCXode)
     {
+        switch (_keyCXode)
+        {
+            case KeyCode.RightArrow:
+                if (forward)
+                {
+                    characterArt.Rotate(0, 180, 0);
+                    forward = false;
+                }            
+                break;
+
+            case KeyCode.LeftArrow:
+                if (!forward)
+                {
+                    characterArt.Rotate(0, 180, 0);
+                    forward = true;
+                }
+                break;
+
+        }
+
         characterArt.Rotate(0, 180, 0);
-        forward = _b;
+        //forward = _b;
     }
 
-    void Update()
+    void StopScript()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && forward)
-        {
-            FlipCharacter(false);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow) && !forward)
-        {
-            FlipCharacter(true);
-        }
+       // UserInputs.UserInput += 
     }
+
+    void Start()
+    {
+        UserInputs.UserInput += FlipCharacter;
+        Endgame.GameOver += StopScript;
+    }
+
 }
---
+++ Star Catcher/Assets/Scripts/Gameplay/Endgame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Endgame : MonoBehaviour {
+
+    public static Action GameOver;
+
+
+    void OnMouseDown()
+    {
+        GameOver();
+    }
+
+}
