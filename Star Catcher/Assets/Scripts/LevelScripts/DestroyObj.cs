aa8ad796bcfd1ec9a64cdcef455fd1a8bf31c6e3
Unknown <mckaymanning@gmail.com>
2016-10-07 11:17:53 -0600

    Replaced Repo StarCatcher
     with the StarCatcher v2 from the rebuilt zip. Trying to push it up to the online master.

---
+++ Star Catcher/Assets/Scripts/LevelScripts/DestroyObj.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class DestroyObj : MonoBehaviour
+{
+
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Star Catcher/Assets/Scripts/endGame.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Endgame : MonoBehaviour
+{
+
+    public static Action GameOver;
+
+
+    void OnMouseDown()
+    {
+        GameOver();
+    }
+
+}
