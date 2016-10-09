3607fedd38d1f087f2bb61634c05310d1a2fe5fd
Unknown <mckaymanning@gmail.com>
2016-10-09 00:05:50 -0600

    Finishing second  sprint
    Finished skybox and rabbit sprite renderer. Can't get the characterartflip to work again. Also Added v03 mac build to the repositroy

---
+++ Star Catcher/Assets/Scripts/ArtFlip.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArtFlip : MonoBehaviour
+{
+
+    public Transform characterArt;
+    public bool forward = true;
+
+    void FlipCharacter(KeyCode _keyCode)
+    {
+        switch (_keyCode)
+        {
+            case KeyCode.LeftArrow:
+                if (forward)
+                {
+                    characterArt.Rotate(0, 180, 0);
+                    forward = false;
+                }
+                break;
+
+            case KeyCode.RightArrow:
+                if (!forward)
+                {
+                    characterArt.Rotate(0, 180, 0);
+                    forward = true;
+                }
+                break;
+        }
+    }
+
+    void StopScript()
+    {
+        newUserInput.UserInput -= FlipCharacter;
+    }
+
+    void Start()
+    {
+        //EndGame.GameOver += StopScript;
+        newUserInput.UserInput += FlipCharacter;
+    }
+}
---
+++ Star Catcher/Assets/Scripts/newUserInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class newUserInput : MonoBehaviour
+{
+
+    public static Action<KeyCode> UserInput;
+
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.RightArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.LeftArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.LeftArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.LeftArrow);
+        }
+    }
+}
