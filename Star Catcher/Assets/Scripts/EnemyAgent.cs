dbce49addfee05d5441933acf957aee7a386aeec
Unknown <mckaymanning@gmail.com>
2016-11-11 01:13:13 -0700

    At Home Work
    Added in point deduction when player makes contact with wolf. Started using the NavMeshAgent to move the wolf towards the player.

---
+++ Star Catcher/Assets/Scripts/EnemyAgent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAgent : MonoBehaviour
+{
+
+    public NavMeshAgent myAgent;
+    public Transform player;
+
+    // Update is called once per frame
+    void OnTriggerStay()
+    {
+        myAgent.destination = player.position;
+    }
+}
---
+++ Star Catcher/Assets/Scripts/SubtractFromScore.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SubtractFromScore : MonoBehaviour
+{
+
+    public int count;
+    public Text countText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        staticVars.yourScore = 0;
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter()
+    {
+        count = count - 3;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Stars: " + count.ToString();
+    }
+
+    void Update()
+    {
+        staticVars.yourScore = count;
+    }
+}
