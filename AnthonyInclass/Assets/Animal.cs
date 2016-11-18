ae55406e5244363a05a0915310fac8295baab7a9
Unknown <mckaymanning@gmail.com>
2016-11-18 13:59:16 -0700

    Inclass Anthony
    Anthony demonstrated overloading and inheritance. I created a new project called AnthonyInclass.

---
+++ AnthonyInclass/Assets/Animal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start () {
+        Walk();
+	}
+
+    void Walk()
+    {
+        print("Walking");
+    }
+}
---
+++ AnthonyInclass/Assets/Overloading.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overloading : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+        Move(50);
+	
+}
+
+    void Move(int speed)
+    {
+        Vector3 vector = new Vector3(speed, 0, 0);
+        transform.position += vector*Time.deltaTime;
+    }
+
+    void Move (string speed)
+    {
+        Vector3 vector = Vector3.zero;
+
+        switch(speed)
+        {
+            case "Fast":
+                vector = new Vector3(100, 0, 0);
+                break;
+
+            case "Slow":
+                vector = new Vector3(1, 0, 0);
+                break;
+        }
+
+        transform.position = vector*Time.deltaTime;
+    }
+}
