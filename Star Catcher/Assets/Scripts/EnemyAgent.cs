893b9c36956b35f3c6f0ae2b96f566f38b33228a
Unknown <mckaymanning@gmail.com>
2016-11-28 00:43:46 -0700

    Wolf Chase Overhaul
    Trying to get the wolf to chase the player for 8 seconds or until the wolf steals stars and then run off the screen to the left. Right now I'm having a hard time getting the wolf to flip between facing the player and the cube.

--- Star Catcher/Assets/Scripts/EnemyAgent.cs
+++ Star Catcher/Assets/Scripts/EnemyAgent.cs
@@ -6,27 +6,81 @@ public class EnemyAgent : MonoBehaviour
 
     public NavMeshAgent myAgent;
     public Transform player;
-    
+    public Transform cube;
     public ParticleSystem Smoke;
     public ParticleSystem Trail;
-    
-    
+    public bool stolen = false;
+    public bool bored = false;
     //public Animator myAnims;
+
+
+    
+    public Vector3 tempPos;
+    public Transform wolf;
+
+
+    public bool canFlipWolf = true;
+
+   void Update ()
+    {
+        FlipOrNot();
+    }
 
     void OnWillRenderObject()
     {
+
         Trail.Play();
-        Chase();
-        Destroy(transform.parent.gameObject, 10);
+        
+        StayOrGo();
+        StartCoroutine(GotBored());
+        
+        //Chase();
+        //Destroy(transform.parent.gameObject, 10);
        // myAgent = GetComponent<NavMeshAgent>();
     }
-    // Update is called once per frame
+
+    void FlipOrNot()
+    {
+        if (stolen == true || bored == true)
+            TurnWolf();
+        else
+            WolfFlipper();
+    }
+
+
+    void StayOrGo ()
+    {
+        if (stolen == true || bored == true)
+        {
+            Runaway();
+            
+        }
+        else
+            Chase();
+       
+
+    }
+
+    IEnumerator GotBored()
+    {
+        yield return new WaitForSeconds(8);
+        bored = true;
+        StayOrGo();
+        yield return new WaitForSeconds(10);
+        Destroy(transform.parent.gameObject);
+    }
+    
     void Chase()
     {
 
         myAgent.destination = player.position;
     }
 
+
+    void Runaway()
+    {
+        myAgent.destination = cube.position;
+    }
     //if (myAgent.isOnOffMeshLink)
     //{
     //    myAnims.SetBool("jump", true);
@@ -42,8 +96,39 @@ public class EnemyAgent : MonoBehaviour
 
     void OnTriggerEnter()
     {
-        Destroy(Trail);
-        Smoke.Play();
-        Destroy(transform.parent.gameObject, .5f);
+        stolen = true;
+        StayOrGo();
+        //Destroy(Trail);
+        //Smoke.Play();
+        Destroy(transform.parent.gameObject, 10);
+    }
+
+    void WolfFlipper()
+    {
+        if (player.transform.position.x < wolf.transform.position.x && canFlipWolf)
+        {
+            wolf.Rotate(0, -180, 0);
+            canFlipWolf = true;
+        }
+        if (player.transform.position.x > wolf.transform.position.x && !canFlipWolf)
+        {
+            wolf.Rotate(0, 180, 0);
+            canFlipWolf = false;
+        }
+    }
+
+
+    void TurnWolf()
+    {
+        if (cube.transform.position.x < wolf.transform.position.x && canFlipWolf)
+        {
+            wolf.Rotate(0, -180, 0);
+            canFlipWolf = true;
+        }
+        if (cube.transform.position.x > wolf.transform.position.x && !canFlipWolf)
+        {
+            wolf.Rotate(0, 180, 0);
+            canFlipWolf = false;
+        }
     }
 }
--- Star Catcher/Assets/WolfArtFlip.cs
+++ Star Catcher/Assets/WolfArtFlip.cs
@@ -5,15 +5,26 @@ public class WolfArtFlip : MonoBehaviour
 {
 
     public GameObject player;
+    public GameObject cube;
     public Vector3 tempPos;
     public Transform wolf;
-
+    
+    
     public bool canFlipWolf = true;
 
     void Update ()
     {
         WolfFlipper();
     }
+
+
+    //void StayOrGo ()
+    //{
+    //    if (stolen == true || bored == true)
+    //        Runaway();
+    //    else
+    //        WolfFlipper();
+    //} 
 
     void WolfFlipper()
     {
@@ -23,6 +34,21 @@ public class WolfArtFlip : MonoBehaviour
             canFlipWolf = false;
         }
         if (player.transform.position.x > wolf.transform.position.x && !canFlipWolf)
+        {
+            wolf.Rotate(0, 180, 0);
+            canFlipWolf = true;
+        }
+    }
+
+
+    void Runaway()
+    {
+        if (cube.transform.position.x < wolf.transform.position.x && canFlipWolf)
+        {
+            wolf.Rotate(0, -180, 0);
+            canFlipWolf = false;
+        }
+        if (cube.transform.position.x > wolf.transform.position.x && !canFlipWolf)
         {
             wolf.Rotate(0, 180, 0);
             canFlipWolf = true;
