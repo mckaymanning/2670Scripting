835d73069bf0d090986f45e50f8fa6fb7ea36163
Unknown <mckaymanning@gmail.com>
2016-11-30 21:00:14 -0700

    Building New Splash Screen
    Added a fade in on the UVU logo. Building a new splash screen.

---
+++ Star Catcher/Assets/Imports/Materials/fades.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fades : MonoBehaviour {
+
+   
+    public GameObject logo;
+
+    void Start ()
+    {
+        logo.SetActive(false);
+        Invoke("fadeIn", 3);
+	}
+
+    void fadeIn()
+    {
+        logo.SetActive(true);
+    }
+	
+
+
+}
